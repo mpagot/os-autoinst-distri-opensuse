836f89c56ec9f8d6eb39d5be9b8756a458362804
Michele Pagot <101880010+mpagot@users.noreply.github.com>
2023-12-13 00:42:53 +0000

    Move back to Dockerfile with dep script

--- .devcontainer/install-dependencies.sh
+++ .devcontainer/install-dependencies.sh
@@ -2,7 +2,7 @@
 zypper dup -y
 
 # dependency suggested in the CONTRIBUTING.md
-zypper in -y os-autoinst-distri-opensuse-deps-worker perl-JSON-Validator gnu_parallel
+zypper in -y os-autoinst-distri-opensuse-deps perl-JSON-Validator gnu_parallel
 
 # other dependency that are needed
 zypper in -y make git vim gcc-c++ libxml2-devel libssh2-devel libexpat-devel dbus-1-devel python311 python311-devel python311-yamllint python311-PyYAML perl-App-cpanminus perl-Code-TidyAll
