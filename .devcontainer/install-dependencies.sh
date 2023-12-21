c9bd795b1e087fc636ced0dd8200784b74e3d87e
Michele Pagot <michele.pagot@suse.com>
2023-12-21 02:10:16 +0100

    Configure Perl Vscode extension
    Install Perl::LanguageServer, configure the plugin with a proper set of import path, move zypper to not interactive mode

--- .devcontainer/install-dependencies.sh
+++ .devcontainer/install-dependencies.sh
@@ -2,7 +2,10 @@
 zypper dup -y
 
 # dependency suggested in the CONTRIBUTING.md
-zypper in -y os-autoinst-distri-opensuse-deps perl-JSON-Validator gnu_parallel
+zypper --non-interactive in os-autoinst-distri-opensuse-deps perl-JSON-Validator gnu_parallel
 
-# other dependency that are needed
-zypper in -y make git vim gcc-c++ libxml2-devel libssh2-devel libexpat-devel dbus-1-devel python311 python311-devel python311-yamllint python311-PyYAML perl-App-cpanminus perl-Code-TidyAll podman
+# other dependency that are needed to run `make test`
+zypper --non-interactive in make git vim gcc-c++ libxml2-devel libssh2-devel libexpat-devel dbus-1-devel python311 python311-devel python311-yamllint python311-PyYAML perl-App-cpanminus perl-Code-TidyAll podman
+
+# dependency needed by the ext: richterger.perl
+zypper --non-interactive in perl-Perl-LanguageServer
